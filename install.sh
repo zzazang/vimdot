93783f8bef8e4603c94a8d1b68bc04375942f95e
zzazang <changho.cha@gmail.com>
2015-04-18 09:59:52 +0900

    mistype fix, mispath fix

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-CWD=$(dirname $0)
+CWD=$(pwd)
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 rm -f ~/.vimrc && ln -s $CWD/vimrc ~/.vimrc
-rm -f ~/.gvimrc && ln -s $CWD/gimrc ~/.gvimrc
+rm -f ~/.gvimrc && ln -s $CWD/gvimrc ~/.gvimrc
 
 vim +PluginInstall +qall
 
